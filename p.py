1959e1fb882c7adab08af7419d2f2563820986c6
Kiran Kumar R <84892935+Kiran9206@users.noreply.github.com>
2021-08-11 16:45:40 +0530

    Stack implementation using list

---
+++ p.py
@@ -0,0 +1,28 @@
+stack = []
+def push():
+    if len(stack)==n:
+        print("The stack is full!")
+    else:
+        for i in range(n):
+            u = int (input("Enter the element : "))
+            a = stack.append(u)
+            print(stack)
+
+def pop_element():
+    if not stack:
+        print("Stack is empty!")
+    else:
+        b=stack.pop()
+        print(b)
+
+n = int(input("How many elements do you want : "))   
+while True:
+    user = int (input("Enter Your Choice 1.push 2.pop 3.exit\n"))
+    if user == 1:
+        push()
+    elif user == 2:
+        pop_element()
+    elif user == 3:
+        break
+    else :
+        print("Enter correct choice!")
